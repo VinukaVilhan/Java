70490c7d50a3a5e9f60d7366f9adffa5415a2ce8
VinukaVilhan <fernandopullevv.22@uom.lk>
2024-09-02 19:57:15 +0530

    first commit

---
+++ test/test.java
@@ -0,0 +1,8 @@
+package test;
+class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+    } 
+}
---
+++ test1/Test.java
@@ -0,0 +1,14 @@
+package test1;
+
+class Test 
+{
+    public static void main(String[] args) 
+    {
+        int age;
+        age = 27;
+
+        System.out.println("I am "+ age + " years old");
+    }
+
+   
+}
